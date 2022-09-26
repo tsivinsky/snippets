a608d53540d52f108e34ef8d2c1338d4a156e68d
Daniil Tsivinsky <daniil@tsivinsky.com>
2022-09-26 22:29:12 +0300

    fix(ComponentPanel): add scroll if needed to docs

--- src/components/ComponentPanel.tsx
+++ src/components/ComponentPanel.tsx
@@ -25,7 +25,9 @@ export const ComponentPanel: React.FC<ComponentPanelProps> = ({
 
   return (
     <div className="p-3 border rounded-md">
-      <ReactMarkdown className="unreset">{component.docs}</ReactMarkdown>
+      <ReactMarkdown className="unreset overflow-x-auto">
+        {component.docs}
+      </ReactMarkdown>
       <div className="flex flex-col md:flex-row items-start gap-2">
         <Button
           className="border border-blue-500 rounded-md py-1 px-3 bg-blue-500/20 text-blue-500 text-lg transition-colors duration-200 hover:bg-transparent"
